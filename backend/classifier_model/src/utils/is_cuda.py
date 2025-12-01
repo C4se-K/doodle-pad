fa5c1933d4f0df0b3add099111881359b9e2c886
kimi_no_shiriai <94500383+C4se-K@users.noreply.github.com>
2025-12-01 02:23:19 -0800

    move to another repo

--- backend/classifier_model/src/utils/is_cuda.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    print("cuda is available.")
-    print("Number of GPUs:", torch.cuda.device_count())
-    for i in range(torch.cuda.device_count()):
-        print(f"gpu {i}: {torch.cuda.get_device_name(i)}")
-else:
-    print("cuda not available, using cpu")
